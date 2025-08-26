9af223b07c42221cb0129b6a1e4eda3f109412e8
fr33tcat <fr33tcat@163.com>
2025-08-26 16:11:22 +0800

    部署 Chirpy 博客

---
+++ Gemfile
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+source "https://rubygems.org"
+
+gem "jekyll", "~> 4.3"
+gem "jekyll-theme-chirpy", "~> 7.3"
+
+platforms :windows do
+  gem "tzinfo", ">= 1", "< 3"
+  gem "tzinfo-data"
+  gem "wdm", "~> 0.2.0"
+end
+
+gem "html-proofer", "~> 5.0", group: :test
---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1756195837',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
